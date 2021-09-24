6e275ecaa3a59652ac8595e22d02079c0aa3d595
Daimel Moreno <Daimel@Jinas-MacBook-Pro.local>
2021-09-24 16:29:03 -0400

    minor settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Router, Switch } from 'react-router-dom'
+
+//components
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        /* Link components if needed */
+        <h1>Memories Book</h1>
       </header>
+      /* Router components if needed */
     </div>
   );
 }
