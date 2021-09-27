959392fac9d95b20dac17598ce8b1bf0bcd04a02
Erika Fomich <erikafomich@gmail.com>
2021-09-27 14:32:54 -0700

    edited config.js url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const APIURL =
 process.env.NODE_ENV === 'production'
-    ? 'https://app-memories-backend.herokuapp.com/'
+    ? 'https://app-memories-backend.herokuapp.com'
     : 'http://localhost:4000';
