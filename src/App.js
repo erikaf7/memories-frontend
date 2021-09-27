4523d879aedbd2a224a04f5b3467c05dae0dec48
Allison Moss <allisonwmoss@icloud.com>
2021-09-27 12:17:42 -0400

    Added edit functionality and tested.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 import { Link, Route, Switch } from 'react-router-dom'
-import Homepage from './components/Homepage';
-import AllEntries from './components/AllEntries';
+
 
 //components
 import ConnectionTestHome from './components/ConnectionTestHome';
 import ConnectionTestList from './components/ConnectionTestList';
 import EntryDetail from './components/EntryDetail';
 import EntryCreate from './components/EntryCreate';
+import Homepage from './components/Homepage';
+import AllEntries from './components/AllEntries';
+import EntryEdit from './components/EntryEdit';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         <Route exact path="/entries" component={AllEntries} />
         <Route exact path="/entries/create" component={EntryCreate} />
         <Route exact path="/entries/:id" component={EntryDetail} />
+        <Route exact path="/entries/:id/edit" component={EntryEdit} />
       </Switch>
     </div>
   );
