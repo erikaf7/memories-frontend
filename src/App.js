1e30d9d086cb3552ab227db5f2aa1d7e2069c2ef
Allison Moss <allisonwmoss@icloud.com>
2021-09-27 10:31:00 -0400

    Resolved conflicts.

--- src/App.js
+++ src/App.js
@@ -11,19 +11,11 @@ import ConnectionTestList from './components/ConnectionTestList';
 function App() {
   return (
     <div className="container">
-      <header>
-        /* Link components if needed */
-        {/* just to test connection can be deleted/changed
-        <Link to="/">Home</Link> |
-        <Link to="/entries">All Entries</Link>
-        */}
-        <h1>Memories Book</h1>
-      </header>
-      /* Router components if needed */
+      <Homepage />
       {/* just to test connection can be deleted/changed */}
       {/* <Switch>*/}
-        {/*<Route exact path="/" component={ ConnectionTestHome } /> */}
-        {/*<Route exact path="/entries" component={ ConnectionTestList } /> */}
+      {/*<Route exact path="/" component={ ConnectionTestHome } /> */}
+      {/*<Route exact path="/entries" component={ ConnectionTestList } /> */}
       {/*</Switch>*/}
     </div>
   );
