950694fed323f40a9b57896e07ced4f18ce9da39
Allison Moss <allisonwmoss@icloud.com>
2021-09-28 12:26:47 -0400

    Added additional styling to nav, view all page, create post form, and edit post form.

--- src/components/EntryCreate.js
+++ src/components/EntryCreate.js
@@ -49,13 +49,26 @@ export default function EntryCreate() {
     return (
         <div className="container">
             <Nav />
-            <h2>Create Entry</h2>
-            {error && <div className="alert alert-danger">Sorry, something went wrong. Please try again.</div>}
-            <EntryForm
-                entry={entry}
-                handleChange={handleChange}
-                handleSubmit={handleSubmit}
-            />
+            <div className="container d-flex align-items-center justify-content-center my-5">
+                <div className="card align-self-center">
+                    <div className="row g-0">
+                        <div className="col-md-6">
+                            <img src="https://images.unsplash.com/photo-1595981234058-a9302fb97229?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" className="img-fluid rounded-start"></img>
+                        </div>
+                        <div className="col-md-6 d-flex align-items-center">
+                            <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                                <h1 className="card-title">create a new entry</h1>
+                                {error && <div className="alert alert-danger">Sorry, something went wrong. Please try again.</div>}
+                                <EntryForm
+                                    entry={entry}
+                                    handleChange={handleChange}
+                                    handleSubmit={handleSubmit}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
         </div>
     )
 }
